ca481ca5e456cef24c2c7807350c9daf5a4f307d
YohanNayanajith <yohannayanajith40@gmail.com>
2025-03-09 02:33:13 +0530

    task/fix product page unload

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,7 +1,8 @@
 import ProductPageClient from './product';
 
 export function generateStaticParams() {
-  return [{ id: "test" }];
+  return [{ id: "1" }, { id: "2" }, { id: "3" }, { id: "4" }, { id: "5" }, { id: "6" }];
+  // return [{ id: "test" }];
 }
 export default async function ProductPage({ params }: { params: { id: string } }) {
   return <ProductPageClient id={params.id} />;
